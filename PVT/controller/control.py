37022c3086207732415886f636bc3c6ceccf1b40
kevin <kjsevill@espol.edu.ec>
2022-06-23 01:07:08 -0500

    Funcion del BO

--- PVT/controller/control.py
+++ PVT/controller/control.py
@@ -1,3 +1,14 @@
 import xlwings as xw
+from PVT.model.BO import BO
 
+
+def main():
+    wb = xw.Book.caller()
+
+
+
+
+if __name__ == "__main__":
+    xw.Book("control.xlsm").set_mock_caller()
+    main()
 
