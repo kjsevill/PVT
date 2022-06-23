7257cf3da583187b28953ca3934e488fa24120c3
kevin <kjsevill@espol.edu.ec>
2022-06-23 00:59:05 -0500

    Funcion del BO

--- PVT/model/BO-Prueba.py
+++ PVT/model/BO-Prueba.py
@@ -1,1 +1,16 @@
-print("HOLA MUNDO")
+def BO(Vpt, Vsc):
+    """"
+    Parameters
+    ---------------
+
+    Vpt:
+        Volumen de fluido a condiciones de reservorio
+
+    Vsc:
+        Volumen de fluido a condiciones Standard
+
+    Returns
+        float number  -> BO
+
+    """
+    return Vpt / Vsc
